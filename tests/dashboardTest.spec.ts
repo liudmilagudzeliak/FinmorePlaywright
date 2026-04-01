a45fb0d551c270d9bb3e205f58c6b6a200a67a5b
liudmilagudzeliak <80971642796l@gmail.com>
2026-04-01 18:04:20 +0300

    dashboardtests

---
+++ tests/dashboardTest.spec.ts
@@ -0,0 +1,143 @@
+import { test, expect } from "@playwright/test";
+
+test.describe('Check dashboard test functions', () => {
+    test.beforeEach(async ({ page }) => {
+        //Create new user and log in before each test
+        await page.goto('/');
+    });
+    test('Check that User can log out from dashboard', async ({ page }) => {
+
+        //Create new account
+        //Click on register link
+        const registerLink = page.getByTestId('switch-to-register-button');
+        await expect(registerLink).toBeVisible({ timeout: 5000 });
+        await registerLink.click();
+        //Fill in the name       
+        const registerName = page.getByTestId('register-name-input');
+        await expect(registerName).toBeVisible({ timeout: 5000 });
+        await expect(registerName).toBeEnabled();
+        await registerName.fill('AUTOTEST');
+        //Fill in the email
+        const emailInput = page.getByTestId('register-email-input');
+        await expect(emailInput).toBeVisible({ timeout: 5000 });
+        await expect(emailInput).toBeEnabled();
+        await emailInput.fill('email@email.com');
+        //Fill in the password
+        const registerPasswordInput = page.getByTestId('register-password-input');
+        await expect(registerPasswordInput).toBeVisible({ timeout: 5000 });
+        await expect(registerPasswordInput).toBeEnabled();
+        await registerPasswordInput.fill('test123');
+        //Fill in the confirm password
+        const registerConfirmPasswordInput = page.getByTestId('register-confirm-password-input');
+        await expect(registerConfirmPasswordInput).toBeVisible({ timeout: 5000 });
+        await expect(registerPasswordInput).toBeEnabled();
+        await registerConfirmPasswordInput.fill('test123');
+        //Click on register button
+        const registerButton = page.getByTestId('register-submit-button');
+        await expect(registerButton).toBeVisible({ timeout: 5000 });
+        await expect(registerButton).toBeEnabled
+        await registerButton.click();
+        //Log out from dashboard
+        //CLick on user settings button
+        const UserSettingsButton = page.getByTestId('user-menu-trigger');
+        await expect(UserSettingsButton).toBeVisible({ timeout: 5000 });
+        await expect(UserSettingsButton).toBeEnabled();
+        await UserSettingsButton.click();
+        //Click on log out button
+        const logoutButton = page.getByTestId('logout-button');
+        await expect(logoutButton).toBeVisible({ timeout: 5000 });
+        await expect(logoutButton).toBeEnabled();
+        await logoutButton.click();
+        //Check that user is logged out and Register page is visible
+        const pageH1 = page.getByTestId('register-title');
+        await expect(pageH1).toBeVisible({ timeout: 6000 });
+        await expect(pageH1).toHaveText('Реєстрація');
+    });
+
+    test('Check that User can enter dohid', async ({ page }) =>  {
+        //Create new account
+        //Click on register link
+        const registerLink = page.getByTestId('switch-to-register-button');
+        await expect(registerLink).toBeVisible({ timeout: 5000 });
+        await registerLink.click();
+        //Fill in the name       
+        const registerName = page.getByTestId('register-name-input');
+        await expect(registerName).toBeVisible({ timeout: 5000 });
+        await expect(registerName).toBeEnabled();
+        await registerName.fill('AUTOTEST');
+        //Fill in the email
+        const emailInput = page.getByTestId('register-email-input');
+        await expect(emailInput).toBeVisible({ timeout: 5000 });
+        await expect(emailInput).toBeEnabled();
+        await emailInput.fill('email@email.com');
+        //Fill in the password
+        const registerPasswordInput = page.getByTestId('register-password-input');
+        await expect(registerPasswordInput).toBeVisible({ timeout: 5000 });
+        await expect(registerPasswordInput).toBeEnabled();
+        await registerPasswordInput.fill('test123');
+        //Fill in the confirm password
+        const registerConfirmPasswordInput = page.getByTestId('register-confirm-password-input');
+        await expect(registerConfirmPasswordInput).toBeVisible({ timeout: 5000 });
+        await expect(registerPasswordInput).toBeEnabled();
+        await registerConfirmPasswordInput.fill('test123');
+        //Click on register button
+        const registerButton = page.getByTestId('register-submit-button');
+        await expect(registerButton).toBeVisible({ timeout: 5000 });
+        await expect(registerButton).toBeEnabled
+        await registerButton.click();
+        //Open new transaction pop-up
+        const addTransactionButton = page.getByTestId('add-transaction-button');
+        await expect(addTransactionButton).toBeVisible({ timeout: 5000 });
+        await expect(addTransactionButton).toBeEnabled();
+        await addTransactionButton.click();
+        //Click income button
+        const incomeButton = page.getByTestId('income-type-button');
+        await expect(incomeButton).toBeVisible({ timeout: 5000 });
+        await expect(incomeButton).toBeEnabled();
+        await incomeButton.click();
+        //Fill in the amount
+        const amountInput = page.getByTestId('transaction-amount-input');
+        await expect(amountInput).toBeVisible({ timeout: 5000 });
+        await expect(amountInput).toBeEnabled();
+        await amountInput.fill('100');
+        //Select category
+        const categorySelect = page.getByTestId('transaction-category-select');
+        const options = categorySelect.getByRole('option');
+        await expect(categorySelect).toBeVisible({ timeout: 5000 });
+        await expect(categorySelect).toBeEnabled();
+        await expect(options).toHaveCount(4);
+        await options.selectOption('Зарплата');
+        //Enter description
+        const descriptionInput = page.getByTestId('transaction-description-input');
+        await expect(descriptionInput).toBeVisible({ timeout: 5000 });
+        await expect(descriptionInput).toBeEnabled();
+        await descriptionInput.fill('test');
+        //Enter date
+        const dateInput = page.getByTestId('transaction-date-input');
+        await expect(dateInput).tobeVisible({ timeout: 5000 });
+        await expect(dateInput).toBeEnabled();
+        await dateInput.fill('2024-06-01');
+        //Select account
+        const accountDropdown = page.getByTestId('transaction-account-select');
+        const accountOptions = accountDropdown.getByRole('option');
+        await expect(accountDropdown).toBeVisible({ timeout: 5000 });
+        await expect(accountDropdown).toBeEnabled();
+        await expect(accountOptions).toHaveCount(4);
+        await accountOptions.selectOption('Готівка');
+        //Add tag
+        const tagInput = page.getByTestId('transaction-tags-input');
+        await expect(tagInput).toBeVisible({ timeout: 5000 });
+        await expect(tagInput).toBeEnabled();
+        await tagInput.fill('testtag');
+        const addTagButton = page.getByTestId('add-tag-button');
+        await expect(addTagButton).toBeVisible({ timeout: 5000 });
+        await expect(addTagButton).toBeEnabled();
+        await addTagButton.click();
+        //Save transaction
+        const saveButton = page.getByTestId('transaction-form-submit');
+        await expect(saveButton).toBeVisible({ timeout: 5000 });
+        await expect(saveButton).toBeEnabled();
+        await saveButton.click();
+
+    });
+});
--- tests/registerPageTest.spec.ts
+++ tests/registerPageTest.spec.ts
@@ -7,15 +7,40 @@ test.describe('Check register test functions', () => {
         await page.goto('/');
         await expect(page).toHaveURL('/');
         await expect(page).toHaveTitle('Повнофункціональний фінансовий менеджер');
+        const registerLink = page.getByTestId('switch-to-register-button');
+        await expect(registerLink).toBeVisible({ timeout: 5000 });
+        await registerLink.click();
     });
 
     test('Check view password icon visability', async ({ page }) => {
         const viewPasswordIcon = page.locator('.lucide-eye').first();
-        const registerPasswordInput = page.getByTestId('register-confirm-password-input');
+        const registerPasswordInput = page.getByTestId('register-password-input');
+        const registerPasswordInputConfirm = page.getByTestId('register-confirm-password-input');
+        //const viewPasswordIconConfirm = page.locator('button:has(svg.lucide-eye)').last();
+        const viewPasswordIconConfirm = page.locator('button:has(svg.lucide-eye)');
+
+        //Check that the view password icon is visible and enabled, and that the password input is visible,
+        //  enabled and has type password. Then click the view password icon and check that the password input has type text.         
         await expect(viewPasswordIcon).toBeVisible({ timeout: 5000 });
+        await expect(registerPasswordInput).toBeVisible({ timeout: 5000 });
+        await expect(registerPasswordInput).toBeEnabled();
         await expect(registerPasswordInput).toHaveAttribute('type', 'password');
+        await (registerPasswordInput).fill('test123');
         await viewPasswordIcon.click();
         await expect(registerPasswordInput).toHaveAttribute('type', 'text');
+
+        // Repeat the same for the confirm password input and view password icon.
+        await expect(registerPasswordInputConfirm).toBeVisible({ timeout: 5000 });
+        await expect(registerPasswordInputConfirm).toBeEnabled();
+        await expect(viewPasswordIconConfirm).toBeVisible({ timeout: 5000 });
+        await expect(viewPasswordIconConfirm).toBeEnabled();
+
+        await expect(registerPasswordInputConfirm).toHaveAttribute('type', 'password');
+        await (registerPasswordInputConfirm).fill('test123');
+        await viewPasswordIconConfirm.click();
+        await expect(registerPasswordInputConfirm).toHaveAttribute('type', 'text');
+
+
         //nth(0) - перший елемент з класом .lucide-eye
 
     });
@@ -36,12 +61,14 @@ test.describe('Check register test functions', () => {
     //expect(message).not.toBe('');
 
     test('Check email input validation browser message', async ({ page }) => {
+        const registerLink = page.getByTestId('switch-to-register-button');
+        await expect(registerLink).toBeVisible({ timeout: 5000 });
+        await registerLink.click();
+            
         const registerName = page.getByTestId('register-name-input');
         await expect(registerName).toBeVisible({ timeout: 5000 });
         await expect(registerName).toBeEnabled();
-        await registerName.fill('');
-
-        const
+        await registerName.fill('test');
 
         const emailInput = page.getByTestId('register-email-input');
         await expect(emailInput).toBeVisible({ timeout: 5000 });
@@ -65,7 +92,7 @@ test.describe('Check register test functions', () => {
         await registerButton.click();
 
         const emailInputErrormessage = await emailInput.evaluate((el: HTMLInputElement) => el.validationMessage);
-        expect(emailInputErrormessage).toBe("Please include an '@' in the email address. 'fdvd' is missing an '@'.");
+        expect(emailInputErrormessage).toBe("Please include an '@' in the email address. 'inavalidemail' is missing an '@'.");
 
 
     });
@@ -89,7 +116,7 @@ test.describe('Check register test functions', () => {
 
         const confirmPasswordErrorMessage = page.getByTestId('confirm-password-error');
         await expect(confirmPasswordErrorMessage).toBeVisible({ timeout: 5000 });
-        await expect(confirmPasswordErrorMessage).toHaveText('Підтвердження пароля обов\'язкове');
+        await expect(confirmPasswordErrorMessage).toHaveText('Підтвердження паролю обов\'язкове');
     });
 
     test('Check registration message for to short name and passworsd', async ({ page }) => {
@@ -103,47 +130,56 @@ test.describe('Check register test functions', () => {
         await expect(passwordInput).toBeEnabled();
         await passwordInput.fill('p');
 
+        const registerButton = page.getByTestId('register-submit-button');
+        await expect(registerButton).toBeVisible({ timeout: 5000 });
+        await registerButton.click();
+
         const nameErrorMessage = page.getByTestId('name-error');
         await expect(nameErrorMessage).toBeVisible({ timeout: 5000 });
         await expect(nameErrorMessage).toHaveText("Ім'я повинно містити мінімум 2 символи");
-
-
-        test('Check registration error message for password mismatch', async ({ page }) => {
-            const registerPasswordInput = page.getByTestId('register-password-input');
-            await expect(registerPasswordInput).toBeVisible({ timeout: 5000 });
-            await expect(registerPasswordInput).toBeEnabled();
-            await registerPasswordInput.fill('test');
-
-            const passwordErrorMessage = page.getByTestId('password-error');
-            await expect(passwordErrorMessage).toHaveText(('Пароль повинен містити мінімум 6 символів'));
-        });
-
-        test('Check registerpage placeholders', async ({ page }) => {
-            const registerName = page.getByTestId('register-name-input');
-            await expect(registerName).toBeVisible({ timeout: 5000 });
-            await expect(registerName).toBeEnabled();
-            await expect(registerName).toHaveAttribute('placeholder', 'Іван Петренко');
-
-            const emailInput = page.getByTestId('register-email-input');
-            await expect(emailInput).toBeVisible({ timeout: 5000 });
-            await expect(emailInput).toBeEnabled();
-            await expect(emailInput).toHaveAttribute('placeholder', 'your@email.com');
-
-            const registerPasswordInput = page.getByTestId('register-password-input');
-            await expect(registerPasswordInput).toBeVisible({ timeout: 5000 });
-            await expect(registerPasswordInput).toHaveAttribute('placeholder', 'Мінімум 6 символів');
-
-            const registerConfirmPasswordInput = page.getByTestId('register-confirm-password-input');
-            await expect(registerConfirmPasswordInput).toBeVisible({ timeout: 5000 });
-            await expect(registerConfirmPasswordInput).toHaveAttribute('placeholder', 'Повторіть пароль');
-        });
-
-        test('Check curreccy drop-down default value', async ({ page }) => {
-            const registercurrencyDropdown = page.getByTestId('register-currency-select');
-            await expect(registercurrencyDropdown).toBeVisible({ timeout: 5000 });
-            await expect(registercurrencyDropdown).toBeEnabled();
-            await expect(registercurrencyDropdown).toHaveValue('UAH');
-            await expect(registercurrencyDropdown.locator('option:checked')).toHaveText('Гривня UAH');
-        });
-
     });
+
+
+    test('Check registration error message for password mismatch', async ({ page }) => {
+        const registerPasswordInput = page.getByTestId('register-password-input');
+        await expect(registerPasswordInput).toBeVisible({ timeout: 5000 });
+        await expect(registerPasswordInput).toBeEnabled();
+        await registerPasswordInput.fill('test');
+
+        const registerButton = page.getByTestId('register-submit-button');
+        await expect(registerButton).toBeVisible({ timeout: 5000 });
+        await registerButton.click();
+
+        const passwordErrorMessage = page.getByTestId('password-error');
+        await expect(passwordErrorMessage).toHaveText(('Пароль повинен містити мінімум 6 символів'));
+    });
+
+    test('Check registerpage placeholders', async ({ page }) => {
+        const registerName = page.getByTestId('register-name-input');
+        await expect(registerName).toBeVisible({ timeout: 5000 });
+        await expect(registerName).toBeEnabled();
+        await expect(registerName).toHaveAttribute('placeholder', 'Іван Петренко');
+
+        const emailInput = page.getByTestId('register-email-input');
+        await expect(emailInput).toBeVisible({ timeout: 5000 });
+        await expect(emailInput).toBeEnabled();
+        await expect(emailInput).toHaveAttribute('placeholder', 'your@email.com');
+
+        const registerPasswordInput = page.getByTestId('register-password-input');
+        await expect(registerPasswordInput).toBeVisible({ timeout: 5000 });
+        await expect(registerPasswordInput).toHaveAttribute('placeholder', 'Мінімум 6 символів');
+
+        const registerConfirmPasswordInput = page.getByTestId('register-confirm-password-input');
+        await expect(registerConfirmPasswordInput).toBeVisible({ timeout: 5000 });
+        await expect(registerConfirmPasswordInput).toHaveAttribute('placeholder', 'Повторіть пароль');
+    });
+
+    test('Check curreccy drop-down default value', async ({ page }) => {
+        const registercurrencyDropdown = page.getByTestId('register-currency-select');
+        await expect(registercurrencyDropdown).toBeVisible({ timeout: 5000 });
+        await expect(registercurrencyDropdown).toBeEnabled();
+        await expect(registercurrencyDropdown).toHaveValue('UAH');
+        await expect(registercurrencyDropdown.locator('option:checked')).toHaveText('Гривня (UAH)');
+    });
+
+});
